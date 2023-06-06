93024f7f766debc468af407935b4a2dc330c3d82
Evgenii-Barannik <radchemsu@gmail.com>
2023-06-06 04:36:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,15 @@ elif [ "$CF_PAGES_BRANCH" == "preview" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
 
+    REPO_URL="https://github.com/Alzymologist/yeast"
+    REPO_DIR="/yeast"
+    SRC_SUBDIR="/src"
+    
+    git clone $REPO_URL $DEST_DIR
+    cd $DEST_DIR$SRC_SUBDIR
+    cargo run --verbose
     zola build
+    
+# This command clones the repository
 
 fi
