e1ee2833b0d92d921476b2dab1b875aab5c70519
Evgenii-Barannik <radchemsu@gmail.com>
2023-06-06 07:00:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,21 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Run the "production" script in `package.json` on the "production" branch
   # "production" should be replaced with the name of your Production branch
 
-    echo "Building from main branch 🔥"
+    echo "Building from main 🔥"
     zola build
 
-elif [ "$CF_PAGES_BRANCH" == "feature-main"] || [ "$CF_PAGES_BRANCH" == "dev-main" ]; then
+elif [ "$CF_PAGES_BRANCH" == "dev-main" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
 
-    echo "Building from development branch 🔥"
+    echo "Building from dev-main 🔥"
+    zola build
+
+elif [ "$CF_PAGES_BRANCH" == "feature-main"]; then
+  # Run the "staging" script in `package.json` on the "staging" branch
+  # "staging" should be replaced with the name of your specific branch
+
+    echo "Building from feature-main 🔥"
     zola build
 
 fi
