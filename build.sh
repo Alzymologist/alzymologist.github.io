b32aaa64a8025128da11fdcbbe7638454d8ad65e
Evgenii-Barannik <radchemsu@gmail.com>
2023-06-06 04:12:37 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "production" ]; then
+  # Run the "production" script in `package.json` on the "production" branch
+  # "production" should be replaced with the name of your Production branch
+
+    zola build
+
+elif [ "$CF_PAGES_BRANCH" == "preview"]; then
+  # Run the "staging" script in `package.json` on the "staging" branch
+  # "staging" should be replaced with the name of your specific branch
+
+    zola build
