4d2077ed562a2cfd07c760d76322a1ed5a54b03d
Evgenii-Barannik <radchemsu@gmail.com>
2023-10-16 20:39:28 +0300

    build.sh update for production deployment

--- build.sh
+++ build.sh
@@ -1,23 +1,27 @@
 # !/bin/bash
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  # Run the "production" script in `package.json` on the "production" branch
-  # "production" should be replaced with the name of your Production branch
-
     echo "Building from main 🔥"
-    zola build
 
-elif [ "$CF_PAGES_BRANCH" == "dev-main" ]; then
-  # Run the "staging" script in `package.json` on the "staging" branch
-  # "staging" should be replaced with the name of your specific branch
-
-    echo "Building from dev-main 🔥"
+    curl https://sh.rustup.rs -sSf | sh -s -- -y # Cargo installation
+    source "$HOME/.cargo/env"
+    apt install graphviz # Graphviz installation
+    
+    REPO_DIR="yeast"
+    REPO_URL="https://github.com/Alzymologist/yeast"
+    REPO_BRANCH="main"
+  
+    mkdir content
+    cp -R content_fresh/* content 
+    mkdir $REPO_DIR
+    git clone --branch $REPO_BRANCH $REPO_URL $REPO_DIR
+    cd $REPO_DIR
+    cargo run --verbose
+    cp -R output/* ../static/yeast-component-output
+    cd .. 
     zola build
 
 elif [ "$CF_PAGES_BRANCH" == "feature-main" ]; then
-  # Run the "staging" script in `package.json` on the "staging" branch
-  # "staging" should be replaced with the name of your specific branch
-
     echo "Building from feature-main 🔥"
 
     curl https://sh.rustup.rs -sSf | sh -s -- -y # Cargo installation
