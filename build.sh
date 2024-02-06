9ab582d0c9ba5fbd5aed37b0bcc9a96174cb3b66
Evgenii-Barannik <radchemsu@gmail.com>
2024-02-07 00:23:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,25 +21,25 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
     # cd .. 
     zola build
 
-elif [ "$CF_PAGES_BRANCH" == "feature-main" ]; then
+elif [ "$CF_PAGES_BRANCH" != "main" ]; then
     echo "Building for STAGING 🔥"
 
     curl https://sh.rustup.rs -sSf | sh -s -- -y # Cargo installation
     source "$HOME/.cargo/env"
-    apt install graphviz # Graphviz installation
+    # apt install graphviz # Graphviz installation
     
-    REPO_DIR="yeast_component"
-    REPO_URL="https://github.com/Alzymologist/yeast"
-    REPO_BRANCH="feature-2"
+    # REPO_DIR="yeast_component"
+    # REPO_URL="https://github.com/Alzymologist/yeast"
+    # REPO_BRANCH="feature-2"
   
     mkdir content
     cp -R content_fresh/* content 
-    mkdir $REPO_DIR
-    git clone --branch $REPO_BRANCH $REPO_URL $REPO_DIR
-    cd $REPO_DIR
-    cargo run -- --staging
-    cp -R output/* ../static/yeast-component-output
-    cd .. 
+    # mkdir $REPO_DIR
+    # git clone --branch $REPO_BRANCH $REPO_URL $REPO_DIR
+    # cd $REPO_DIR
+    # cargo run -- --staging
+    # cp -R output/* ../static/yeast-component-output
+    # cd .. 
     zola build
 
 fi
